d8cda1ef927b67183fc35107b28bc2fd96d9d2a3
BEugen <b-eugen@yandex.ru>
2012-04-21 23:55:08 +0400

    add

---
+++ RtpConfig/GetSingleShiberSetup.proc.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[GetSingleShiberSetup]
+	@rtpid int = 0
+AS
+	Select SingleSequence.id, SingleSequence.sequencenumber, SingleSequence.shibernumber, RtpSignalsGroup.signalgroupdescription, ShiberSetup.timeOpen, ShiberSetup.timeClose, ShiberSetup.timeBetwenShiber,
+           ShiberSetup.reopenCountMax
+    from (SingleSequence LEFT OUTER JOIN ShiberSetup ON SingleSequence.shibernumber = ShiberSetup.shibernumber)
+          LEFT OUTER JOIN RtpSignalsGroup ON RtpSignalsGroup.signalattrnumber = SingleSequence.shibernumber
+    WHERE SingleSequence.rtpid = @rtpid
+    order by SingleSequence.sequencenumber
+RETURN 0
---
+++ RtpConfig/Scripts/Post-Deployment/AddShiberSingleSequence.sql
@@ -0,0 +1,122 @@
+﻿-- =============================================
+-- Script Template
+-- =============================================
+insert into SingleSequence ( rtpid,
+	sequencenumber,	
+	shibernumber)
+values(0, 1, 1)
+
+insert into SingleSequence ( rtpid,
+	sequencenumber,	
+	shibernumber)
+values(0, 2, 2)
+
+insert into SingleSequence ( rtpid,
+	sequencenumber,	
+	shibernumber)
+values(0, 3, 3)
+
+insert into SingleSequence ( rtpid,
+	sequencenumber,	
+	shibernumber)
+values(0, 4, 4)
+
+insert into SingleSequence ( rtpid,
+	sequencenumber,	
+	shibernumber)
+values(0, 5, 5)
+
+insert into SingleSequence ( rtpid,
+	sequencenumber,	
+	shibernumber)
+values(0, 6, 6)
+
+insert into SingleSequence ( rtpid,
+	sequencenumber,	
+	shibernumber)
+values(0, 7, 7)
+
+insert into SingleSequence ( rtpid,
+	sequencenumber,	
+	shibernumber)
+values(0, 8, 8)
+
+insert into SingleSequence ( rtpid,
+	sequencenumber,	
+	shibernumber)
+values(0, 9, 9)
+
+insert into SingleSequence ( rtpid,
+	sequencenumber,	
+	shibernumber)
+values(0, 10, 10)
+
+insert into SingleSequence ( rtpid,
+	sequencenumber,	
+	shibernumber)
+values(0, 11, 11)
+
+insert into SingleSequence ( rtpid,
+	sequencenumber,	
+	shibernumber)
+values(0, 12, 12)
+
+insert into SingleSequence ( rtpid,
+	sequencenumber,	
+	shibernumber)
+values(0, 13, 13)
+
+insert into SingleSequence ( rtpid,
+	sequencenumber,	
+	shibernumber)
+values(0, 14, 14)
+
+insert into SingleSequence ( rtpid,
+	sequencenumber,	
+	shibernumber)
+values(0, 15, 15)
+
+insert into SingleSequence ( rtpid,
+	sequencenumber,	
+	shibernumber)
+values(0, 16, 16)
+
+insert into SingleSequence ( rtpid,
+	sequencenumber,	
+	shibernumber)
+values(0, 17, 17)
+
+insert into SingleSequence ( rtpid,
+	sequencenumber,	
+	shibernumber)
+values(0, 18, 18)
+
+insert into SingleSequence ( rtpid,
+	sequencenumber,	
+	shibernumber)
+values(0, 19, 19)
+
+insert into SingleSequence ( rtpid,
+	sequencenumber,	
+	shibernumber)
+values(0, 20, 20)
+
+insert into SingleSequence ( rtpid,
+	sequencenumber,	
+	shibernumber)
+values(0, 21, 21)
+
+insert into SingleSequence ( rtpid,
+	sequencenumber,	
+	shibernumber)
+values(0, 22, 22)
+
+insert into SingleSequence ( rtpid,
+	sequencenumber,	
+	shibernumber)
+values(0, 23, 23)
+
+insert into SingleSequence ( rtpid,
+	sequencenumber,	
+	shibernumber)
+values(0, 24, 24)
