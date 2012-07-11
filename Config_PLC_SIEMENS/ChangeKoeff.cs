fb3a9fb5bfd4445ad5254a21cb1ecd6d93c868c8
BEugen <b-eugen@yandex.ru>
2012-07-11 15:38:37 +0000

    Fix error

--- Config_PLC_SIEMENS/ChangeKoeff.cs
+++ Config_PLC_SIEMENS/ChangeKoeff.cs
@@ -11,6 +11,8 @@ namespace RtpWagoConf
 {
     public partial class ChangeKoeff : Form
     {
+        private double _kOpen = 0.0;
+        private double _kClose = 0.0;
         public ChangeKoeff()
         {
             InitializeComponent();
@@ -18,12 +20,12 @@ namespace RtpWagoConf
 
         public double KoeffOpen
         {
-            get { return inp_koeff_open.Value; }
+            get { return _kOpen; }
             set { inp_koeff_open.Value = value; }
         }
         public double KoeffClose
         {
-            get { return inp_koeff_close.Value; }
+            get { return _kClose; }
             set { inp_koeff_close.Value = value; }
         }
 
@@ -31,12 +33,14 @@ namespace RtpWagoConf
 
         private void InpKoeffOpenKeyUp(object sender, KeyEventArgs e)
         {
-            inp_koeff_close.Value = 1.0 - inp_koeff_open.Value;
-        }
+           inp_koeff_close.Value = _kClose= 1.0 - inp_koeff_open.Value;
+            _kOpen = 1.0 - _kClose;
+        }  
 
         private void InpKoeffCloseKeyUp(object sender, KeyEventArgs e)
         {
-            inp_koeff_open.Value = 1.0 - inp_koeff_close.Value;
+           inp_koeff_open.Value = _kOpen = 1.0 - inp_koeff_close.Value;
+           _kClose = 1.0 - _kOpen;
         }
 
     }
